737c5db96d00b0f2c5b6d4927552a09760dd53ee
r-tarnowski <radek.github@wp.pl>
2022-12-10 23:41:49 +0100

    Initial commit

---
+++ src/DependencyInversionPrinciple.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <boost/lexical_cast.hpp>
+
+
+using std::cout;
+using std::endl;
+
+void printHeader() {
+   cout << endl;
+   cout << "================================================" << endl;
+   cout << "Design Patterns in Modern C++ by Dmitri Nesteruk" << endl;
+   cout << "SOLID, Dependency Inversion Principle" << endl;
+   cout << "================================================" << endl;
+   cout << endl;
+}
+
+int main(int argc, char *argv[]) {
+   printHeader();
+
+   cout << endl;
+   return 0;
+}
